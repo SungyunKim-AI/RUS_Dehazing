13bb08ca97303614a59619f4c4040715c17dbb3d
Jeonghoon Kim <amsa9210@konkuk.ac.kr>
2021-09-10 00:22:37 +0900

    train

---
+++ BPP-Net/HazeDataset.py
@@ -0,0 +1,88 @@
+import torch
+import cv2
+from PIL import Image
+import PIL
+import torchvision.transforms.functional as F
+import glob
+import random
+
+
+def to_tensor(img):
+    img_t = F.to_tensor(img).float()
+    return img_t
+    
+def load_item(haze, clear):
+        hazy_image = cv2.imread(haze)
+        clear_image = cv2.imread(clear)
+        
+        hazy_image = Image.fromarray(hazy_image)
+        clear_image = Image.fromarray(clear_image)
+ 
+        hazy_resize = hazy_image.resize((512,512), resample=PIL.Image.BICUBIC)
+        clear_resize = clear_image.resize((512,512), resample=PIL.Image.BICUBIC)
+        
+        hazy_resize = to_tensor(hazy_resize).cuda()
+        clear_resize = to_tensor(clear_resize).cuda()
+        
+        return hazy_resize, clear_resize
+
+class O_Haze_Train_Dataset(torch.utils.data.Dataset):
+    def __init__(self,path):
+        super().__init__()
+        images_clear_path = path+'/clear/*.jpg'
+        images_hazy_path = path+'/hazy/*.jpg'
+        self.images_clear_list=glob.glob(images_clear_path)
+        self.images_hazy_list =glob.glob(images_hazy_path)
+    
+    def __len__(self):
+        return len(self.images_clear_list)
+    
+    def __getitem__(self,index):
+        haze, clear = load_item(self.images_hazy_list[index], self.images_clear_list[index])
+        return haze, clear
+        
+class RESIDE_Beta_Train_Dataset(torch.utils.data.Dataset):
+    def __init__(self,path):
+        super().__init__()
+        images_clear_path = path+'/clear/*.jpg'
+        self.images_clear_list = glob.glob(images_clear_path)
+        
+        images_hazy_folders_path = path+'/hazy/*/'
+        self.images_hazy_lists = []
+        images_hazy_folders=glob.glob(images_hazy_folders_path)
+        for images_hazy_folder in images_hazy_folders:
+            self.images_hazy_lists.append(glob.glob(images_hazy_folder+'*.jpg'))
+            
+        self.folders_count = len(self.images_hazy_lists)
+        self.images_count = len(self.images_hazy_lists[0])
+        
+    def __len__(self):
+        return self.folders_count * self.images_count
+    
+    def __getitem__(self,index):
+        haze, clear = load_item(self.images_hazy_lists[index//self.images_count][index%self.images_count],
+                                self.images_clear_list[index%self.images_count])
+        return haze, clear
+
+
+class Train_Dataset(torch.utils.data.Dataset):
+    def __init__(self, path):
+        super().__init__()
+        self.RBTD = RESIDE_Beta_Train_Dataset(path+'/RESIDE-beta/train')
+        self.OHTD = O_Haze_Train_Dataset(path+'/O-Haze/train')
+        
+    def __len__(self):
+        return self.RBTD.__len__() + self.OHTD.__len__()
+
+    def __getitem__(self, index):
+        if index >= self.RBTD.__len__():
+            haze, clear = self.OHTD[index-self.RBTD.__len__()]
+        else:
+            haze, clear = self.RBTD[index]
+        return haze, clear
+    
+if __name__ == '__main__':
+    t = Train_Dataset('D:/data')
+    print(t.__len__())
+    while(True):
+        t[random.randint(0,50000)]
--- BPP-Net/train.py
+++ BPP-Net/train.py
@@ -1,82 +1,13 @@
 import torch
-import random, os, glob, wandb
-import cv2
-import numpy as np
-import PIL
-from PIL import Image
+import wandb
+from tqdm import tqdm
 
 from models import *
 from metrics import *
-import torchvision.transforms.functional as F
 from torch.utils.data import DataLoader
+from HazeDataset import RESIDE_Beta_Train_Dataset
 
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, haze_list, dehaze_list, augment=False):
-        super().__init__()
-        self.augment = augment
-        self.haze_list = haze_list
-        self.dehaze_list = dehaze_list
-        
-    def __len__(self):
-        return 45
-
-    def __getitem__(self, index):
-        try:
-            item = self.load_item(index)
-        except:
-            print('loading error: ' + self.haze_list[index])
-            item = self.load_item(0)
-            
-
-        return item
-
-
-    def load_item(self, index):
-        val = 1024*2           #crop size i.e hight and width
-        size_data = 25         #depends on the no. of training images in the dataset
-        height_data = 4657     #heigth of the training images
-        width_data = 2833      #width of the training images
-
-        numx = random.randint(0, height_data-val)
-        numy = random.randint(0, width_data-val)
-
-        haze_image = cv2.imread(self.haze_list[index%size_data])
-        dehaze_image = cv2.imread(self.dehaze_list[index%size_data])
-        haze_image = Image.fromarray(haze_image)
-        dehaze_image = Image.fromarray(dehaze_image)
-
-        haze_crop=haze_image.crop((numx, numy, numx+val, numy+val))
-        dehaze_crop=dehaze_image.crop((numx, numy, numx+val, numy+val))
- 
-        haze_crop = haze_crop.resize((512,512), resample=PIL.Image.BICUBIC)
-        dehaze_crop = dehaze_crop.resize((512,512), resample=PIL.Image.BICUBIC)
-
-        haze_crop = np.array(haze_crop)
-        dehaze_crop = np.array(dehaze_crop)
-        haze_crop = cv2.cvtColor(haze_crop, cv2.COLOR_BGR2YCrCb)
-        dehaze_crop = cv2.cvtColor(dehaze_crop, cv2.COLOR_BGR2YCrCb)
-        haze_crop = self.to_tensor(haze_crop).cuda()
-        dehaze_crop = self.to_tensor(dehaze_crop).cuda()
-        
-        return haze_crop.cuda(), dehaze_crop.cuda()
-    
-    def to_tensor(self, img):
-        img_t = F.to_tensor(img).float()
-        return img_t
-
-
-    def create_iterator(self, batch_size):
-        while True:
-            sample_loader = DataLoader(
-                dataset=self,
-                batch_size=batch_size,
-                drop_last=True
-            )
-
-            for item in sample_loader:
-                yield item
-
-def train(max_epochs, model):
+def train(max_epochs, model, train_loader):
     
     for epoch in range(max_epochs):
         i=1
@@ -84,7 +15,8 @@ def train(max_epochs, model):
         ssim_epoch = 0.0
         psnr_epoch = 0.0
         unet_epoch = 0.0
-        for haze_images, dehaze_images, in train_loader:
+        for batch in tqdm(train_loader):
+            haze_images, dehaze_images = batch
             with torch.autograd.set_detect_anomaly(True):
                 unet_loss, dis_loss, mse, ssim, psnr = model.process(haze_images.cuda(), dehaze_images.cuda())
                 model.backward(unet_loss.cuda(), dis_loss.cuda())
@@ -122,20 +54,14 @@ if __name__ == '__main__':
     config = wandb.config
     
     # ================ DataLoader ================
-    path_of_train_hazy_images = 'train/hazy/*.jpg'
-    path_of_train_gt_images = 'train/GT/*.jpg'
 
-    images_paths_train_gt=glob.glob(path_of_train_gt_images)
-    image_paths_train_hazy=glob.glob(path_of_train_hazy_images)
-
-    train_dataset = Dataset(image_paths_train_hazy, images_paths_train_gt, augment=False)
-
+    train_dataset = RESIDE_Beta_Train_Dataset('D:/data/RESIDE-beta/train')
     train_loader = DataLoader(
                 dataset=train_dataset,
                 batch_size=1,
                 num_workers=0,
                 drop_last=True,
-                shuffle=False
+                shuffle=True
             )
     
     # ================ Creating the model ================
@@ -148,14 +74,10 @@ if __name__ == '__main__':
     
     # ================ Training ================
     epochs = 150
-    train(epochs, DUNet)
+    train(epochs, DUNet, train_loader)
     
     
     # ================ Saving weights ================
     path_of_generator_weight = 'weight/generator.pth'  #path for storing the weights of genertaor
     path_of_discriminator_weight = 'weight/discriminator.pth'  #path for storing the weights of discriminator
     DUNet.save_weight(path_of_generator_weight,path_of_discriminator_weight)
-    
-    path_of_generator_weight = 'weight/generator.pth'  #path where the weights of genertaor are stored
-    path_of_discriminator_weight = 'weight/discriminator.pth'  #path where the weights of discriminator are stored
-    DUNet.load('weights/new/in all/u_' + str(21) + '.pth','weights/new/in all/d_' + str(21) + '.pth')
