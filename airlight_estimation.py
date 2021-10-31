e595675608d240e3b244b3981aacb1fe488321a8
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-10-31 13:32:00 +0900

    commit

--- airlight_estimation.py
+++ airlight_estimation.py
@@ -4,32 +4,47 @@ Image Haze Removal Using Airlight White Correction, Local Light Filter, and Aeri
 Yan-Tsung Peng et al.
 """
 import cv2
-import math
 import numpy as np
-from scipy.stats import rv_discrete
+import matplotlib.pyplot as plt
 
-def RGB2HSV(image):
+
+# Airlight White Correction (AWC)
+def AWC_Module(image, color_cast_threshold=5):
+    # 1. RGB to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    hue, S, V = cv2.split(hsv)        # Hue, Saturation, Value
-    return hue
+    hue, _, _ = cv2.split(hsv)
     
-def PMF_H(hue):
-    xk = np.arange(360)
-    print(np.histogram(hue))
-    pmf = rv_discrete.pmf(xk)
+    # 2. Hue histogram labeling
+    val, cnt = np.unique(hue, return_counts=True)
+    img_size = hue.shape[0]*hue.shape[1]
     
-    k = math.floor(x)
-    xlogy = lambda x, y : x*math.log(y)
-    xlog1py = lambda x, y : x*math.log1p(y)
-    logpmf = lambda n, p : math.lgamma(n+1)-math.lgamma(k+1)-math.lgamma(n-k+1)+xlogy(k,p)+xlog1py(n-k,-p)
+    P_H = cnt / img_size    # PMF        
+    T_H = img_size / 360    # ?
     
-    P_H = math.exp(logpmf(x,n,p))
-    T_H = image.shape / 360
+    binarized_hue = np.zeros((hue.shape[0], hue.shape[1]), int)
+    for i, h in enumerate(val):
+        row, col = np.where(hue == h)
+        
+        for j in row:
+            for k in col:
+                if P_H[i] > T_H:
+                    binarized_hue[j][k] = 1
     
-    if P_H > T_H:
-        return 1
-    else:
-        return 0
-
+    # 3. Color cast attenuation
+    num_labels, labels = cv2.connectedComponents(binarized_hue)   # Connected Component Labeling (CCL)
+    
+    # 4. HSV to RGB
+    
+def LLF_Module():
+    # 1. PMF of minimum channel calculation
+    
+    # 2. 1D minimum filter
+    
+    # 3. Airlight color estimation
+  
 if __name__ == "__main__":
-    image = cv2.imread()
+    image = cv2.imread("testImage.jpeg")
+    # image = cv2.imread("testImage.jpeg", cv2.IMREAD_GRAYSCALE)
+    hue = getHue(image)
+    PMF_H(hue)
+    
