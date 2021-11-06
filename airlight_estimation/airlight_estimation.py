9e28d2c40783200e22ad4bbf28ed9ea8938f4dae
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2021-11-06 19:06:46 +0900

    Update airlight_estimation.py

--- airlight_estimation/airlight_estimation.py
+++ airlight_estimation/airlight_estimation.py
@@ -6,36 +6,57 @@ Yan-Tsung Peng et al.
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
+def show_plt(x, y):
+    plt.bar(x, y, align='center')
+    plt.xlabel('deg.')
+    plt.xlim([0, 359])
+    plt.ylabel('prob.')
+    for i in range(len(y)):
+        plt.vlines(x[i], 0, y[i])
+    
+    plt.show()
 
 # Airlight White Correction (AWC)
 def AWC_Module(image, color_cast_threshold=5):
     # 1. RGB to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    hue, _, _ = cv2.split(hsv)
+    hue, saturation, value = cv2.split(hsv)
     
     # 2. Hue histogram labeling
     val, cnt = np.unique(hue, return_counts=True)
-    img_size = hue.shape[0]*hue.shape[1]
-    
-    P_H = cnt / img_size    # PMF        
-    T_H = img_size / 360    # ?
-    
-    binarized_hue = np.zeros((hue.shape[0], hue.shape[1]), int)
-    for i, h in enumerate(val):
-        row, col = np.where(hue == h)
-        
-        for j in row:
-            for k in col:
-                if P_H[i] > T_H:
-                    binarized_hue[j][k] = 1
+    img_size = hue.shape[0] * hue.shape[1]
+    prob = cnt / img_size    # PMF
+    T_H = 1 / 360
+    binarized_hue = np.zeros(hue.shape, int)
+    for i in range(hue.shape[0]):
+        for j in range(hue.shape[1]):
+            idx = np.where(val == hue[i][j])
+            if prob[idx] > T_H:
+                binarized_hue[i][j] = 1
     
     # 3. Color cast attenuation
-    num_labels, labels = cv2.connectedComponents(binarized_hue)   # Connected Component Labeling (CCL)
+    binarized_img = binarized_hue.astype('uint8')
+    num_labels, _ = cv2.connectedComponents(binarized_img)   # Connected Component Labeling (CCL)
+    if num_labels < color_cast_threshold:   # has color cast
+        flatten_s = saturation.flatten()
+        idx = int(len(flatten_s) / 100)
+        least_idx = np.argpartition(flatten_s, idx)
+        least_value = flatten_s[least_idx[:idx]].mean()
+        
+        I_s = saturation - least_value
+        I_s[I_s < 0] = 0
+        saturation = I_s.astype('uint8')
     
     # 4. HSV to RGB
+    hsv = cv2.merge((hue, saturation, value))
+    rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB) 
     
-def LLF_Module():
+    return rgb
+        
+    
+# def LLF_Module():
     # 1. PMF of minimum channel calculation
     
     # 2. 1D minimum filter
@@ -44,7 +65,5 @@ def LLF_Module():
   
 if __name__ == "__main__":
     image = cv2.imread("testImage.jpeg")
-    # image = cv2.imread("testImage.jpeg", cv2.IMREAD_GRAYSCALE)
-    hue = getHue(image)
-    PMF_H(hue)
+    AWC_Module(image)
     
