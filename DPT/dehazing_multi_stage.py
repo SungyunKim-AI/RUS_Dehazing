93608a32621ce7b916798d136bf819d167dcdc71
Jeonghoon Kim <amsa9210@konkuk.ac.kr>
2021-11-16 17:42:12 +0900

    commit

--- DPT/dehazing_multi_stage.py
+++ DPT/dehazing_multi_stage.py
@@ -1,9 +1,11 @@
 from math import nan
+from numpy.lib.function_base import diff
 import torch
+import skvideo.measure as m
 import cv2
 import numpy as np
 import csv
-from torch.nn.functional import l1_loss
+from torch.nn.functional import l1_loss, threshold
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from airlight_estimation import Airlight_Module, show_plt
@@ -34,10 +36,10 @@ def test_with_D_Hazy_NYU_notation(model, test_loader, device):
     stage = 100
     model.eval()
     psnr_sum, ssim_sum = 0.0, 0.0
-    step_beta = 0.05
+    step_beta = 0.01
     
-    a = 0.0018
-    b = 3.95
+    a = 0.0012
+    b = 3.7
     
     a_sum, b_sum= 0,0
     for cnt, batch in tqdm(enumerate(test_loader)):
@@ -337,15 +339,15 @@ def test_with_RESIDE_notation(model, test_loader, device):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-def test(model, test_loader, device, show=False, write_csv=False):
+def test_stop_before_zero(model, test_loader, device, show=False, write_csv=False):
     entropy_module = Entropy_Module()
-    stage = 100
+    stage = 300
     model.eval()
     psnr_sum, ssim_sum = 0.0, 0.0
-    step_beta = 0.01
+    step_beta = 0.001
     print(f'beta_per_stage = {step_beta}')
-    a = 0.0018
-    b = 3.95
+    a = 0.0012
+    b = 3.7
     for batch in tqdm(test_loader):
         
         last_etp = 0
@@ -468,6 +470,143 @@ def test(model, test_loader, device, show=False, write_csv=False):
     batch_num = len(test_loader)
     print(f'mean_psnr = {psnr_sum/batch_num}, mean_ssim = {ssim_sum/batch_num}')
 
+def test_stop_when_threshold(model, test_loader, device, show=False, write_csv=False):
+    entropy_module = Entropy_Module()
+    airlight_module = Airlight_Module()
+    stage = 1000
+    model.eval()
+    psnr_sum, ssim_sum = 0.0, 0.0
+    step_beta = 0.001
+    thresh_hold= 0.001
+    print(f'beta_per_stage = {step_beta}')
+    a = 0.0012
+    b = 3.7
+    for batch in tqdm(test_loader):
+        
+        hazy_images, clear_images, airlight_images, input_name= batch
+        
+        csv_wr = None
+        csv_file = None
+        if write_csv:
+            csv_name = input_name[0][:-3]+'csv'
+            csv_file = open(csv_name,'w',newline='')
+            csv_wr = csv.writer(csv_file)
+            csv_wr.writerow(['stage','step_beta','cur_etp','diff_etp','psnr','ssim'])
+        
+        
+        with torch.no_grad():
+            hazy_images = hazy_images.to(device)
+            clear_images = clear_images.to(device)
+            airlight_images = airlight_images.to(device)
+            _, init_depth = model.forward(hazy_images)
+            _, init_clear_depth = model.forward(clear_images)
+            
+        init_hazy = (hazy_images[0] * 0.5 + 0.5).detach().cpu().numpy().transpose(1,2,0)
+        init_clear = (clear_images[0] * 0.5 + 0.5).detach().cpu().numpy().transpose(1,2,0)
+        init_airlight = (airlight_images[0] * 0.5 + 0.5).detach().cpu().numpy().transpose(1,2,0)
+        
+        last_etp = entropy_module.get_entropy((init_hazy*255).astype(np.uint8))
+        last_niqe = m.niqe(cv2.cvtColor(init_hazy,cv2.COLOR_BGR2GRAY))
+        print(last_niqe)
+        exit(0)
+        
+        init_depth = init_depth.detach().cpu().numpy().transpose(1,2,0)
+        init_depth = a*init_depth+b
+        
+        init_clear_depth = init_clear_depth.detach().cpu().numpy().transpose(1,2,0)
+        init_clear_depth = a*init_clear_depth+b
+        
+        _psnr = 0
+        _ssim = 0
+        step=0
+        
+        depth = init_depth.copy()
+        prediction = None
+        last_depth = None
+        airlight = None
+        finding_max=True
+        
+        for i in range(1,stage+1):
+            
+            last_depth = depth.copy()
+            
+            
+            with torch.no_grad():
+                hazy_images = hazy_images.to(device)
+                _, depth = model.forward(hazy_images)
+                            
+            hazy = (hazy_images[0] * 0.5 + 0.5).detach().cpu().numpy().transpose(1,2,0)
+            #airlight= (airlight_module.getAirlight((hazy*255).astype(np.uint8))/255).astype(np.float32)
+            airlight = init_airlight
+            
+            depth = depth.detach().cpu().numpy().transpose(1,2,0)
+            depth = a*depth+b
+            depth = np.minimum(depth,last_depth)
+            
+            trans = np.exp(depth * step_beta * -1)
+            
+            prediction = (hazy-airlight)/(trans+e) + airlight
+            prediction = np.clip(prediction,0,1)
+            hazy_images = torch.Tensor(((prediction-0.5)/0.5).transpose(2,0,1)).unsqueeze(0)
+            
+            cur_etp = entropy_module.get_entropy((prediction*255).astype(np.uint8))
+            diff_etp = cur_etp - last_etp
+            
+            _psnr = psnr(init_clear,prediction)
+            _ssim = ssim(init_clear,prediction).item()
+            
+            if (diff_etp<thresh_hold or i==stage) and finding_max:
+                step = i-1
+                psnr_sum +=_psnr
+                ssim_sum +=_ssim
+                print(f'last_stage = {step}')
+                print(f'last_psnr= {_psnr}')
+                print(f'last_ssim= {_ssim}')
+                print(f'last_etp   = {last_etp}')
+                finding_max=False
+                if write_csv==False:
+                    last_etp = cur_etp
+                    break
+                
+            last_etp = cur_etp
+            
+            if write_csv:
+                csv_wr.writerow([i,step_beta,cur_etp,diff_etp,_psnr,_ssim])
+                
+        if write_csv:
+            csv_file.close()
+        
+        trans = np.exp(init_depth * (step_beta*step) * -1)
+        one_shot_prediction = (init_hazy-init_airlight)/(trans+e) + init_airlight
+        one_shot_prediction = np.clip(one_shot_prediction,0,1)
+        _psnr = psnr(init_clear,one_shot_prediction)
+        _ssim = ssim(init_clear,one_shot_prediction).item()
+        clear_etp = entropy_module.get_entropy((init_clear*255).astype(np.uint8))
+        print(f'clear_etp  = {clear_etp}')
+        print(f'one-shot: beta = {step_beta*step}, psnr = {_psnr}, ssim={_ssim}')
+        
+        prediction = cv2.cvtColor(prediction.astype(np.float32),cv2.COLOR_BGR2RGB)
+        init_hazy_img = cv2.cvtColor(init_hazy,cv2.COLOR_RGB2BGR)
+        init_clear_img = cv2.cvtColor(init_clear,cv2.COLOR_RGB2BGR)
+        init_airlight_img = cv2.cvtColor(init_airlight,cv2.COLOR_RGB2BGR)
+        airlight_img = cv2.cvtColor(airlight,cv2.COLOR_RGB2BGR)
+        one_shot_prediction = cv2.cvtColor(one_shot_prediction.astype(np.float32),cv2.COLOR_BGR2RGB)
+        
+        if show:
+            cv2.imshow('final depth', depth/10)
+            cv2.imshow('last stage prediction',prediction)
+            cv2.imshow("init_hazy",init_hazy_img)
+            cv2.imshow("init_clear",init_clear_img)
+            cv2.imshow("init_airlight",init_airlight_img)
+            cv2.imshow("final_airlight",airlight_img)
+            cv2.imshow("init_depth", init_depth/10)
+            cv2.imshow("init_clear_depth", init_clear_depth/10)
+            cv2.imshow('one_shot_prediction',one_shot_prediction)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+    
+    batch_num = len(test_loader)
+    print(f'mean_psnr = {psnr_sum/batch_num}, mean_ssim = {ssim_sum/batch_num}')
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"device: {device}")
@@ -494,13 +633,13 @@ if __name__ == '__main__':
     model.to(device)
     
     
-    dataset_test = D_Hazy_NYU_Dataset_With_Notation('D:/data/D_Hazy_NYU/train',[net_w,net_h],printName=False)
+    dataset_test = NYU_Dataset('D:/data/NYU/val',[net_w,net_h],printName=True,returnName=True)
     loader_test = DataLoader(
                 dataset=dataset_test,
                 batch_size=1,
                 num_workers=0,
                 drop_last=False,
-                shuffle=False)
+                shuffle=True)
     
-    test_with_D_Hazy_NYU_notation(model,loader_test,device)
-    #test(model,loader_test,device,show=False,write_csv=False)
+    #test_with_D_Hazy_NYU_notation(model,loader_test,device)
+    test_stop_when_threshold(model,loader_test,device,show=False,write_csv=False)
